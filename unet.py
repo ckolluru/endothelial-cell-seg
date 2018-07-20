a944a9d7e2aba8069a23eca1e48b41e3fc773240
ChaitanyaKolluru1 <cxk340@case.edu>
2018-07-20 09:03:00 +0530

    Upload software

---
+++ unet.py
@@ -0,0 +1,120 @@
+import os 
+import numpy as np
+from keras.models import *
+from keras.layers import Input, merge, Conv2D, MaxPooling2D, UpSampling2D, Dropout, Cropping2D
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as keras
+from data import *
+
+class myUnet(object):
+
+    def __init__(self, img_rows = 256, img_cols = 256):
+
+        self.img_rows = img_rows
+        self.img_cols = img_cols
+
+    def load_data(self):
+
+        mydata = dataProcess(self.img_rows, self.img_cols)
+        imgs_train, imgs_mask_train = mydata.load_train_data()
+        imgs_test = mydata.load_test_data()
+        return imgs_train, imgs_mask_train, imgs_test
+
+    def get_unet(self):
+
+        inputs = Input((self.img_rows, self.img_cols,1))
+        
+
+        conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+        print ("conv2 shape:",conv2.shape)
+        conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+        print ("conv2 shape:",conv2.shape)
+        pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+        print ("pool2 shape:",pool2.shape)
+
+        conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+        print ("conv3 shape:",conv3.shape)
+        conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+        print ("conv3 shape:",conv3.shape)
+        pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+        print ("pool3 shape:",pool3.shape)
+
+        conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+        conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+        drop4 = Dropout(0.5)(conv4)
+        pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+        conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+        conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+        drop5 = Dropout(0.5)(conv5)
+
+        up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
+        merge6 = merge([drop4,up6], mode = 'concat', concat_axis = 3)
+        conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+        conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+        up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+        merge7 = merge([conv3,up7], mode = 'concat', concat_axis = 3)
+        conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+        conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+        up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+        merge8 = merge([conv2,up8], mode = 'concat', concat_axis = 3)
+        conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+        conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+        conv8 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+        conv9 = Conv2D(1, 1, activation = 'sigmoid')(conv8)
+
+        model = Model(input = inputs, output = conv9)
+
+        model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+        return model
+
+
+    def train(self):
+
+        print("loading data")
+        imgs_train, imgs_mask_train, imgs_test = self.load_data()
+        print("loading data done")
+        model = self.get_unet()
+        print("got unet")
+
+        model_checkpoint = ModelCheckpoint('UNet_Neuronal_Cell_Cropped.hdf5', monitor='loss',verbose=1, save_best_only=True)
+
+        print('Loading weights...')
+        model.load_weights('/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/UNet_Neuronal_Cell_Cropped.hdf5')
+        print('Weights loaded...')
+        
+        print('Fitting model...')
+        model.fit(imgs_train, imgs_mask_train, batch_size=5, epochs=10, verbose=1,validation_split=0.2, shuffle=True, callbacks=[model_checkpoint])
+
+        print('predict test data')
+        imgs_mask_test = model.predict(imgs_test, batch_size=1, verbose=1)
+        np.save('/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/results/imgs_mask_test.npy', imgs_mask_test)
+
+    def save_img(self):
+
+        print("array to image")
+        imgs = np.load('/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/results/imgs_mask_test.npy')
+        for i in range(imgs.shape[0]):
+            img = imgs[i]
+            img = array_to_img(img)
+            img.save("/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/results/%d.jpg"%(i))
+
+
+
+
+if __name__ == '__main__':
+    myunet = myUnet()
+    myunet.train()
+    myunet.save_img()
+
+
+
+
+
+
+
+
