34d6fedaab788e87b6d427dfb07ac63cb0db7639
ChaitanyaKolluru1 <cxk340@case.edu>
2018-07-27 02:57:45 +0530

    Code cleaning
    Former-commit-id: 9cc00a7804f9544768159aea3ec1251c5d081851

--- data.py
+++ data.py
@@ -2,284 +2,83 @@ from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_a
 import numpy as np 
 import os
 import glob
-import cv2
-from libtiff import TIFF
 import re
 
 def natural_keys(text):
-    
-    # Assumes that the path is of the form
-    # home/cxk340/tensorflow/.../1.tif
-    # Any changes in path will need a change here
+
+    # Assumes that the path is of the form /home/<case ID>/.../1.tif
     c = re.split('(/\d+)', text)
     return int(c[1].split('/')[1])
 
-class myAugmentation(object):
-    
-    """
-    A class used to augmentate image
-    Firstly, read train image and label seperately, and then merge them together for the next process
-    Secondly, use keras preprocessing to augmentate image
-    Finally, seperate augmentated image apart into train image and label
-    """
-
-    def __init__(self, train_path="/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/data/train/image",
-                 label_path="/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/data/train/label", 
-                 merge_path="/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/data/train/merge",
-                 aug_merge_path="/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/data/train/aug_merge",
-                 aug_train_path="/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/data/train/aug_train",
-                 aug_label_path="/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/data/train/aug_label",
-                 img_type="tif"):
-        
-        """
-        Using glob to get all .img_type from path
-        """
-
-        self.train_imgs = glob.glob(train_path+"/*."+img_type)
-        self.label_imgs = glob.glob(label_path+"/*."+img_type)
-
-        #Sort based on file name which is a number
-        self.train_imgs.sort(key=natural_keys)
-        self.label_imgs.sort(key=natural_keys)
-
-        self.train_path = train_path
-        self.label_path = label_path
-        self.merge_path = merge_path
-        self.img_type = img_type
-        self.aug_merge_path = aug_merge_path
-        self.aug_train_path = aug_train_path
-        self.aug_label_path = aug_label_path
-        self.slices = len(self.train_imgs)
-        self.datagen = ImageDataGenerator(
-                                    rotation_range=0.2,
-                                    width_shift_range=0.05,
-                                    height_shift_range=0.05,
-                                    shear_range=0.05,
-                                    zoom_range=0.05,
-                                    horizontal_flip=True,
-                                    fill_mode='nearest')
-
-
-    def Augmentation(self):
-
-        """
-        Start augmentation.....
-        """
-        trains = self.train_imgs
-        labels = self.label_imgs
-        path_train = self.train_path
-        path_label = self.label_path
-        path_merge = self.merge_path
-        imgtype = self.img_type
-        path_aug_merge = self.aug_merge_path
-        if len(trains) != len(labels) or len(trains) == 0 or len(trains) == 0:
-            print ("trains can't match labels")
-            return 0
-        for i in range(len(trains)):
-            img_t = load_img(path_train+"/"+str(i+1)+"."+imgtype)
-            img_l = load_img(path_label+"/"+str(i+1)+"."+imgtype)
-            x_t = img_to_array(img_t)
-            x_l = img_to_array(img_l)
-            x_t[:,:,2] = x_l[:,:,0]
-            img_tmp = array_to_img(x_t)
-            img_tmp.save(path_merge+"/"+str(i+1)+"."+imgtype)
-            img = x_t
-            img = img.reshape((1,) + img.shape)
-            savedir = path_aug_merge + "/" + str(i+1)
-            if not os.path.lexists(savedir):
-                os.mkdir(savedir)
-            self.doAugmentate(img, savedir, str(i+1))
-
-
-    def doAugmentate(self, img, save_to_dir, save_prefix, batch_size=1, save_format='tif', imgnum=30):
-
-        """
-        augmentate one image
-        """
-        datagen = self.datagen
-        i = 0
-
-        for batch in datagen.flow(img,
-                          batch_size=batch_size,
-                          save_to_dir=save_to_dir,
-                          save_prefix=save_prefix,
-                          save_format=save_format):
-            i += 1
-            if i > imgnum:
-                break
-
-    def splitMerge(self):
-
-        """
-        split merged image apart
-        """
-        path_merge = self.aug_merge_path
-        path_train = self.aug_train_path
-        path_label = self.aug_label_path
-        for i in range(self.slices):
-            path = path_merge + "/" + str(i+1)
-            train_imgs = glob.glob(path+"/*."+self.img_type)
-            savedir = path_train + "/" + str(i+1)
-            if not os.path.lexists(savedir):
-                os.mkdir(savedir)
-            savedir = path_label + "/" + str(i+1)
-            if not os.path.lexists(savedir):
-                os.mkdir(savedir)
-            for imgname in train_imgs:
-                midname = imgname[imgname.rindex("/")+1:imgname.rindex("."+self.img_type)]
-                img = cv2.imread(imgname)
-                img_train = img[:,:,2]#cv2 read image rgb->bgr
-                img_label = img[:,:,0]
-
-                #cv2.imwrite(path_train+"/"+str(i+1)+"/"+midname+"_train"+"."+self.img_type,img_train)
-                #cv2.imwrite(path_label+"/"+str(i+1)+"/"+midname+"_label"+"."+self.img_type,img_label)
-
-                cv2.imwrite(path_train+"/"+midname+"_train"+"."+self.img_type,img_train)
-                cv2.imwrite(path_label+"/"+midname+"_label"+"."+self.img_type,img_label)
-
-    def splitTransform(self):
-
-        """
-        split perspective transform images
-        """
-        #path_merge = "transform"
-        #path_train = "transform/data/"
-        #path_label = "transform/label/"
-        path_merge = "deform/deform_norm2"
-        path_train = "deform/train/"
-        path_label = "deform/label/"
-        train_imgs = glob.glob(path_merge+"/*."+self.img_type)
-        for imgname in train_imgs:
-            midname = imgname[imgname.rindex("/")+1:imgname.rindex("."+self.img_type)]
-            img = cv2.imread(imgname)
-            img_train = img[:,:,2]#cv2 read image rgb->bgr
-            img_label = img[:,:,0]
-            cv2.imwrite(path_train+midname+"."+self.img_type,img_train)
-            cv2.imwrite(path_label+midname+"."+self.img_type,img_label)
-
-
-
 class dataProcess(object):
 
-    def __init__(self, out_rows, out_cols, data_path = "/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/data/train/aug_train",
-                 label_path = "/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/data/train/aug_label", 
-                 test_path = "/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/data/test",
-                 npy_path = "/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/data/npydata",
-                 img_type = "tif"):
+    def __init__(self, img_height, img_width, username):
+        self.img_height = img_height
+        self.img_width = img_width
+        self.username = username
 
-        """
+    def load_EC_data(self):
+
+        # Augment the training images and labels, because 34 training images is a small number of images for deep learning methods
+        datagen_args = dict(rotation_range=0.2,
+                            width_shift_range=0.05,
+                            height_shift_range=0.05,
+                            shear_range=0.05,
+                            zoom_range=0.05,
+                            horizontal_flip=True,
+                            fill_mode='nearest',
+                            rescale=1./255)
         
-        """
+        image_datagen = ImageDataGenerator(**datagen_args)
+        labels_datagen = ImageDataGenerator(**datagen_args)
 
-        self.out_rows = out_rows
-        self.out_cols = out_cols
-        self.data_path = data_path
-        self.label_path = label_path
-        self.img_type = img_type
-        self.test_path = test_path
-        self.npy_path = npy_path
+        # Load training images and labels from the respective directories
+        image_generator = image_datagen.flow_from_directory('/home/' + self.username + '/endothelial-cell-seg/data/EC/train/image/', color_mode='grayscale', class_mode=None, seed=1, batch_size=32)
+        labels_generator = labels_datagen.flow_from_directory('/home/' + self.username + '/endothelial-cell-seg/data/EC/train/label/', color_mode='grayscale', class_mode=None, seed=1, batch_size=32)
 
-    def create_train_data(self):
-        i = 0
-        print('-'*30)
-        print('Creating training images...')
-        print('-'*30)
-        imgs = glob.glob(self.data_path+"/*."+self.img_type)
-        labs = glob.glob(self.label_path+"/*."+self.img_type)
+        # Find all test images from the data folder
+        self.test_imgs_list = glob.glob('/home/' + self.username + '/endothelial-cell-seg/data/EC/test/*.jpg')
 
-        print(len(imgs), len(labs))
-        imgdatas = np.ndarray((len(imgs),self.out_rows,self.out_cols,1), dtype=np.uint8)
-        imglabels = np.ndarray((len(imgs),self.out_rows,self.out_cols,1), dtype=np.uint8)
+        # Sort so that the list is 1.tif, 2.tif etc. and not 1.tif, 11.tif etc.
+        self.test_imgs_list.sort(key=natural_keys)
 
-        for imgname, labname in zip(imgs, labs):
-            midname = imgname[imgname.rindex("/")+1:]
-            midlabname = labname[labname.rindex("/")+1:]
-            img = load_img(self.data_path + "/" + midname,grayscale = True)
-            label = load_img(self.label_path + "/" + midlabname,grayscale = True)
-            img = img_to_array(img)
-            label = img_to_array(label)
-            #img = cv2.imread(self.data_path + "/" + midname,cv2.IMREAD_GRAYSCALE)
-            #label = cv2.imread(self.label_path + "/" + midname,cv2.IMREAD_GRAYSCALE)
-            #img = np.array([img])
-            #label = np.array([label])
-            imgdatas[i] = img
-            imglabels[i] = label
-            if i % 100 == 0:
-                print('Done: {0}/{1} images'.format(i, len(imgs)))
-            i += 1
-        print('loading done')
-        print(self.npy_path + '/imgs_train.npy')
-        print(np.size(imgdatas))
-        print(np.size(imglabels))
-        np.save(self.npy_path + '/imgs_train.npy', imgdatas)
-        np.save(self.npy_path + '/imgs_mask_train.npy', imglabels)
-        print('Saving to .npy files done.')
-
-    def create_test_data(self):
-        i = 0
-        print('-'*30)
-        print('Creating test images...')
-        print('-'*30)
-        imgs = glob.glob(self.test_path+"/*."+self.img_type)
-        print(len(imgs))
-        imgdatas = np.ndarray((len(imgs),self.out_rows,self.out_cols,1), dtype=np.uint8)
-        for imgname in imgs:
-            print(imgname)
-            midname = imgname[imgname.rindex("/")+1:]
-            img = load_img(self.test_path + "/" + midname,grayscale = True)
-            img = img_to_array(img)
-            #img = cv2.imread(self.test_path + "/" + midname,cv2.IMREAD_GRAYSCALE)
-            #img = np.array([img])
-            imgdatas[i] = img
-            i += 1
-        print('loading done')
-        np.save(self.npy_path + '/imgs_test.npy', imgdatas)
-        print('Saving to imgs_test.npy files done.')
-
-    def load_train_data(self):
-        print('-'*30)
-        print('load train images...')
-        print('-'*30)
-        imgs_train = np.load(self.npy_path+"/imgs_train.npy")
-        imgs_mask_train = np.load(self.npy_path+"/imgs_mask_train.npy")
-        imgs_train = imgs_train.astype('float32')
-        imgs_mask_train = imgs_mask_train.astype('float32')
-        imgs_train /= 255
-        #mean = imgs_train.mean(axis = 0)
-        #imgs_train -= mean
-        imgs_mask_train /= 255
-        imgs_mask_train[imgs_mask_train > 0.5] = 1
-        imgs_mask_train[imgs_mask_train <= 0.5] = 0
-        return imgs_train,imgs_mask_train
-
-    def load_test_data(self):
-        print('-'*30)
-        print('load test images...')
-        print('-'*30)
-        imgs_test = np.load(self.npy_path+"/imgs_test.npy")
-        imgs_test = imgs_test.astype('float32')
-        imgs_test /= 255
-        #mean = imgs_test.mean(axis = 0)
-        #imgs_test -= mean
+        # Read test image files and load them into a numpy array
+        imgs_test_stack = np.zeros((len(self.test_imgs_list), 256, 256))
+        for i in np.arange(len(self.test_imgs_list)):
+            imgs_test_stack[i,:,:] = img_to_array(load_img(self.test_imgs_list[i]))[:,:,1]/255
         
-        for i in range(imgs_test.shape[0]):
-            img = imgs_test[i]
-            img = array_to_img(img)
-            img.save("/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/test_examples/%d.jpg"%(i))
-    
-        return imgs_test
+        print('%d EC test images found' %(len(self.test_imgs_list)))
+        imgs_test_stack = np.expand_dims(imgs_test_stack, axis=-1)
 
-#srun --x11 -p gpuk40 --gres=gpu:2 -N 1 -c 12 --mem=40g --time=100:00:00 --pty /bin/bash
-# module load singularity tensorflow
+        return image_generator, labels_generator, imgs_test_stack
 
-if __name__ == "__main__":
+    def load_neuronal_data(self):
 
-    aug = myAugmentation()
-    aug.Augmentation()
-    aug.splitMerge()
-    #aug.splitTransform()
-    mydata = dataProcess(256,256)
-    mydata.create_train_data()
-    mydata.create_test_data()
-    imgs_train,imgs_mask_train = mydata.load_train_data()
-    print (imgs_train.shape,imgs_mask_train.shape)
+        # The neuron image dataset is (512,512) in size. The images are cropped to (256, 256) and saved in train_crop and test_crop folders.
+
+        # Augment the training images and labels, because 30 training images is a small number of images for deep learning methods.
+        datagen_args = dict(rotation_range=0.2,
+                            width_shift_range=0.05,
+                            height_shift_range=0.05,
+                            shear_range=0.05,
+                            zoom_range=0.05,
+                            horizontal_flip=True,
+                            fill_mode='nearest',
+                            rescale=1./255)
+        
+        image_datagen = ImageDataGenerator(**datagen_args)
+        labels_datagen = ImageDataGenerator(**datagen_args)
+
+        # Load training images and labels from the respective directories
+        image_generator = image_datagen.flow_from_directory('/home/' + self.username + '/endothelial-cell-seg/data/neuronal/train_crop/image/', color_mode='grayscale', class_mode=None, seed=1, batch_size=32)
+        labels_generator = labels_datagen.flow_from_directory('/home/' + self.username + '/endothelial-cell-seg/data/neuronal/train_crop/label/', color_mode='grayscale', class_mode=None, seed=1, batch_size=32)
+
+        # Load testing images and labels from the respective directories
+        image_validation_generator = image_datagen.flow_from_directory('/home/' + self.username + '/endothelial-cell-seg/data/neuronal/test_crop/image/', color_mode='grayscale', class_mode=None, seed=1, batch_size=32)
+        labels_validation_generator = labels_datagen.flow_from_directory('/home/' + self.username + '/endothelial-cell-seg/data/neuronal/test_crop/label/', color_mode='grayscale', class_mode=None, seed=1, batch_size=32)
+        
+        return image_generator, labels_generator, image_validation_generator, labels_validation_generator
+
+if __name__ == '__main__':
+
+    mydata = dataProcess(256,256, 'cxk340')
--- unet.py
+++ unet.py
@@ -1,44 +1,49 @@
 import os 
 import numpy as np
 from keras.models import *
-from keras.layers import Input, merge, Conv2D, MaxPooling2D, UpSampling2D, Dropout, Cropping2D
+from keras.layers import Input, merge, Conv2D, MaxPooling2D, UpSampling2D, Dropout, Cropping2D, concatenate
 from keras.optimizers import *
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras import backend as keras
 from data import *
+import argparse
 
 class myUnet(object):
 
-    def __init__(self, img_rows = 256, img_cols = 256):
+    def __init__(self,args):
 
-        self.img_rows = img_rows
-        self.img_cols = img_cols
+        # Copy command line arguments into object's variables
+        self.pre_train = args.pre_train
+        self.train = args.train
+        self.use_pre_train = args.use_pre_train
+        self.test = args.test
+        self.user = args.u
 
     def load_data(self):
 
-        mydata = dataProcess(self.img_rows, self.img_cols)
-        imgs_train, imgs_mask_train = mydata.load_train_data()
-        imgs_test = mydata.load_test_data()
-        return imgs_train, imgs_mask_train, imgs_test
+        # Load training and test data for the network
+        mydata = dataProcess(256, 256, self.user)
+        imgs_train, imgs_train_labels, imgs_test = mydata.load_EC_data()
+        return imgs_train, imgs_train_labels, imgs_test
+
+    def load_pre_train_data(self):
+
+        # Load pre-training train and validation data for the network
+        mydata = dataProcess(256, 256, self.user)
+        imgs_train, imgs_train_labels, imgs_validation, imgs_validation_labels = mydata.load_neuronal_data()
+        return imgs_train, imgs_train_labels, imgs_validation, imgs_validation_labels
 
     def get_unet(self):
 
-        inputs = Input((self.img_rows, self.img_cols,1))
-        
+        inputs = Input((256, 256, 1))        
 
         conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
-        print ("conv2 shape:",conv2.shape)
         conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
-        print ("conv2 shape:",conv2.shape)
         pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-        print ("pool2 shape:",pool2.shape)
 
         conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
-        print ("conv3 shape:",conv3.shape)
         conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
-        print ("conv3 shape:",conv3.shape)
         pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-        print ("pool3 shape:",pool3.shape)
 
         conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
         conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
@@ -50,71 +55,120 @@ class myUnet(object):
         drop5 = Dropout(0.5)(conv5)
 
         up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
-        merge6 = merge([drop4,up6], mode = 'concat', concat_axis = 3)
+        merge6 = concatenate([drop4, up6], axis=3)
         conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
         conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
 
         up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
-        merge7 = merge([conv3,up7], mode = 'concat', concat_axis = 3)
+        merge7 = concatenate([conv3, up7], axis=3)
         conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
         conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
 
         up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
-        merge8 = merge([conv2,up8], mode = 'concat', concat_axis = 3)
+        merge8 = concatenate([conv2, up8], axis=3)
         conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
         conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
         conv8 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
         conv9 = Conv2D(1, 1, activation = 'sigmoid')(conv8)
 
-        model = Model(input = inputs, output = conv9)
+        model = Model(inputs = inputs, outputs = conv9)
 
         model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
 
+        model.summary()
+
         return model
 
+    def train_and_test(self):
 
-    def train(self):
+        # Pre-training
+        if self.pre_train:
+            print('-' * 30)
+            print('Pre-training on neuronal cell cryo-EM images')
+            print('-' * 30)
+            
+            print('Loading pre-train data (Neuron cells in cryo-EM images)')
+            imgs_train, imgs_train_labels, imgs_validation, imgs_validation_labels = self.load_pre_train_data()
+            print('Loaded pre-train data (Neuron cells in cryo-EM images) \n')
 
-        print("loading data")
-        imgs_train, imgs_mask_train, imgs_test = self.load_data()
-        print("loading data done")
-        model = self.get_unet()
-        print("got unet")
+            # Get the neural network architecture
+            print('Loading network architecture')
+            model = self.get_unet()
+            print('Loaded network architecture \n')
 
-        model_checkpoint = ModelCheckpoint('UNet_Neuronal_Cell_Cropped.hdf5', monitor='loss',verbose=1, save_best_only=True)
+            # Create a checkpoint to save the network weights to a file. Loss on the validation set will be monitored
+            model_checkpoint = ModelCheckpoint('unet_pretrain.hdf5', monitor='val_loss',verbose=1, save_best_only=True)
 
-        print('Loading weights...')
-        model.load_weights('/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/UNet_Neuronal_Cell_Cropped.hdf5')
-        print('Weights loaded...')
-        
-        print('Fitting model...')
-        model.fit(imgs_train, imgs_mask_train, batch_size=5, epochs=10, verbose=1,validation_split=0.2, shuffle=True, callbacks=[model_checkpoint])
+            # Fit the model to the pre-train data
+            print('Fitting model to pre-train dataset')
+            model.fit_generator(zip(imgs_train, imgs_train_labels), validation_data=(zip(imgs_validation, imgs_validation_labels)), steps_per_epoch=20, validation_steps=10, epochs=20, verbose=1, shuffle=True, callbacks=[model_checkpoint])
 
-        print('predict test data')
-        imgs_mask_test = model.predict(imgs_test, batch_size=1, verbose=1)
-        np.save('/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/results/imgs_mask_test.npy', imgs_mask_test)
+        # Train the network 
+        if self.train:
+            print('-' * 30)
+            print('Training on EC microscopy images')
+            print('-' * 30)
 
-    def save_img(self):
+            print('Loading training data (EC cells in microscopy images)')
+            imgs_train, imgs_train_labels, imgs_test = self.load_data()
+            print('Loaded training data (EC cells in microscopy images) \n')
 
-        print("array to image")
-        imgs = np.load('/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/results/imgs_mask_test.npy')
-        for i in range(imgs.shape[0]):
-            img = imgs[i]
-            img = array_to_img(img)
-            img.save("/home/cxk340/tensorflow/unet_batch_mode_endothelial_cell_segmentation/results/%d.jpg"%(i))
+            # Get the neural network architecture
+            print('Loading network architecture')
+            model = self.get_unet()
+            print('Loaded network architecture \n')
 
+            # Create a checkpoint to save the network weights to a file. Loss on the validation set will be monitored
+            model_checkpoint = ModelCheckpoint('unet_train.hdf5', monitor='loss',verbose=1, save_best_only=True)
 
+            if self.use_pre_train:
+                print('Loading weights from the pre-trained network')
+                model.load_weights('/home/' + self.user + '/endothelial-cell-seg/unet_pretrain.hdf5')
+                print('Loaded weights from the pre-trained network \n')
 
+            # Fit the model to the training data
+            print('Fitting model to train dataset')
+            print('TODO: Fix steps_per_epoch and validation_steps - make sure all training examples are used')
+            model.fit_generator(zip(imgs_train, imgs_train_labels), steps_per_epoch=20, epochs=20, validation_steps=10, verbose=1, shuffle=True, callbacks=[model_checkpoint])
+
+        # Test the network
+        if self.test:
+            print('-' * 30)
+            print('Test on unseen EC microscopy images')
+            print('-' * 30)
+
+            # Predict on the test images
+            imgs_test_predictions = model.predict(imgs_test, batch_size=1, verbose=1)
+            print('Predicted on test EC images')
+
+            # Save predictions to the results folder
+            print('Saving predictions on test images to results folder in the current directory')
+            for i in np.arange(imgs_test_predictions.shape[0]):
+                img_test_prediction = array_to_img(imgs_test_predictions[i,:,:])
+                img_test_prediction.save('/home/' + self.user + '/endothelial-cell-seg/results/%d.jpg' %(i))
 
 if __name__ == '__main__':
-    myunet = myUnet()
-    myunet.train()
-    myunet.save_img()
 
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='U-Net for EC cell segmentation')
+    parser.add_argument('--pre_train', default=1, type=int,
+                        help='Pre-train the network to segment neurons in cryo-EM images (0/1, default 1). Images are usually (512, 512) but have been cropped to (256, 256). Step results in a weights file unet_pretrain.hdf5')
+    parser.add_argument('--train', default=1, type=int,
+                        help='Train the network to segment EC cells in microscopy images (0/1, default 1). Images are (256, 256). Step results in a weights file unet_train.hdf5')
+    parser.add_argument('--use_pre_train', default=1, type=int,
+                        help='Use the network weights from pre-training as a starting point (0/1, default 1). Needs a weights file in the current directory called unet_pretrain.hdf5')
+    parser.add_argument('--test', default=1, type=int,
+                        help='Segment EC cells in a held-out test set of microscopy images (0/1, default 1)')
+    parser.add_argument('--u', default='cxk340', type=str,
+                        help='Case username (example cxk340). Download files from Github to your folder on the HPC. Data read/write will then be done from the corresponding folders.')
+    args = parser.parse_args()
 
+    print('\nUsing the following command line arguments:')
+    print(args)
+    print('\n')
 
+    # Make an object of myUnet class
+    myunet = myUnet(args)
 
-
-
-
-
+    # Train and test the U-Net network as needed
+    myunet.train_and_test()
